8f33f637ac62dff4f8a6ebcda6c82908f94a9b1a
darcythomas <github@darcy.io>
2014-06-24 14:05:43 +1200

    solution-ed up the sudoku

---
+++ source/CS/Sudoku/sudoku.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        public Sudoku(string board)
+        {
+        }
+
+        public void Solve()
+        {
+        }
+
+        // Returns a string representing the current state of the board
+        // Don't spend too much time on this method; flag someone from staff
+        // if you are.
+        public string Board()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
